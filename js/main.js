e3684c172ccbc257ef242b099547c9c3a8829aaa
Philip Protogeridis <ph.prot@gmail.com>
2014-01-25 01:14:16 +0200

    Removed Excess JS

--- js/main.js
+++ js/main.js
@@ -17,59 +17,6 @@ $(document).ready(function(){
 		triggerOnce: true
 	}
 
-	$('#flickr-feed').jflickrfeed({
-		limit: 20,
-		qstrings: {
-			id: '94624116@N08'
-		},
-		itemTemplate: 
-		'<li class="gallery-thumb">' +
-		'<a target="_blank" href="{{link}}"><img src="{{image}}" alt="{{title}}" /></a>' +
-		'</li>'
-	});
-
-	(function() {
-	$.fn.bgscroll = $.fn.bgScroll = function( options ) {
-		
-		if( !this.length ) return this;
-		if( !options ) options = {};
-		if( !window.scrollElements ) window.scrollElements = {};
-		
-		for( var i = 0; i < this.length; i++ ) {
-			
-			var allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-			var randomId = '';
-			for( var l = 0; l < 5; l++ ) randomId += allowedChars.charAt( Math.floor( Math.random() * allowedChars.length ) );
-			
-				this[ i ].current = 0;
-				this[ i ].scrollSpeed = options.scrollSpeed ? options.scrollSpeed : 70;
-				this[ i ].direction = options.direction ? options.direction : 'h';
-				window.scrollElements[ randomId ] = this[ i ];
-				
-				eval( 'window[randomId]=function(){var axis=0;var e=window.scrollElements.' + randomId + ';e.current -= 1;if (e.direction == "h") axis = e.current + "px 0";else if (e.direction == "v") axis = "0 " + e.current + "px";else if (e.direction == "d") axis = e.current + "px " + e.current + "px";$( e ).css("background-position", axis);}' );
-														 
-				setInterval( 'window.' + randomId + '()', options.scrollSpeed ? options.scrollSpeed : 70 );
-			}
-			
-			return this;
-		}
-})(jQuery);
-
-	
- 	
-
-
-
-	$('#intro').waypoint(function(direction) {
-	$("#speakers-value").animateNumber(01);
-	$("#delegates-value").animateNumber(20);
-	$("#volunteers-value").animateNumber(20);
-	$("#venue-value").animateNumber(40);
-	$("#workshops-value").animateNumber(55);
-	$("#universities-value").animateNumber(10);
-
-}, {offset:0});
-
 
 });
 
